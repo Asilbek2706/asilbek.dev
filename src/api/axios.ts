c68eebdc8c54f96bba4765e34c46e6d4714cbac2
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-02-16 11:02:41 +0500

    path is changed

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://64.227.185.18/api/v1',
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default api;
