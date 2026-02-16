e8f5f4384ff67be480389a0648087f6dd8c0a2a5
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-02-16 17:36:48 +0500

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // API so'rovlarini yo'naltirish
+      '/api': {
+        target: 'http://64.227.185.18', // Backend manziling
+        changeOrigin: true,            // Serverga so'rov localhostdan emas, targetdan kelayotgandek ko'rsatadi
+        secure: false,                 // Agar HTTPS muammosi bo'lsa, buni chetlab o'tadi
+        rewrite: (path) => path.replace(/^\/api/, '/api/v1') // Agar kerak bo'lsa yo'lni o'zgartiradi
+      }
+    }
+  }
 })
