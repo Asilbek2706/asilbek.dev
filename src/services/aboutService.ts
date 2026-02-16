752a1341750c2d76c328fa3cf1def17b483b3548
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-02-16 09:26:42 +0500

    v3 was hidden

--- src/services/aboutService.ts
+++ src/services/aboutService.ts
@@ -2,7 +2,7 @@ import api from "../api/axios.ts";
 
 export const getAboutInfo = async () => {
     try {
-        const response = await api.get('/about');
+        const response = await api.get('/about/');
         return response.data.data[0] || null;
     } catch (error) {
         console.error("Xatolik bo'ldi, lekin loaderni to'xtatamiz:", error);
