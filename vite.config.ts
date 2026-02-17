14f05b158448d0e17044f209662f0f821762da18
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-02-17 13:42:22 +0500

    project packages is updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react"; // Asl pluginni qaytardik
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    // Allowed Hosts sozlamasi shu yerga qo'shiladi
     allowedHosts: [
       'asilbek-karomatov.dev',
       '64.227.185.18',
