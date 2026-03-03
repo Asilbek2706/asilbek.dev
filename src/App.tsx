2f902d8ada22f8832694eb3593b1ffad7f0c3ce6
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-03-03 12:41:09 +0500

    Scroll smoothing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import Lenis from 'lenis';
 import Layout from './components/Layout/Layout';
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
@@ -6,8 +8,27 @@ import Projects from "./pages/Projects/Projects";
 import Contact from "./pages/Contact/Contact";
 import ScrollToTop from "./components/Shared/Utils/ScrollToTop.tsx";
 
-
 function App() {
+    useEffect(() => {
+        const lenis = new Lenis({
+            duration: 1.4,
+            easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+            wheelMultiplier: 1.1,
+            touchMultiplier: 2,
+            infinite: false,
+        });
+
+        function raf(time: number) {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
+        }
+
+        requestAnimationFrame(raf);
+
+        return () => {
+            lenis.destroy();
+        };
+    }, []);
 
     return (
         <Layout>
